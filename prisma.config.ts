d689c81d5272fe282d9f8e30b009bcf5884a102b
mmanikandap0512 <manikandaprabhu0512@gmail.com>
2026-03-26 16:07:27 +0530

    update: prisma config file

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  engine: "classic",
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
     url: env("DATABASE_URL"),
