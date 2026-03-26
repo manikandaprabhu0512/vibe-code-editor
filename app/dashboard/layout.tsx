c8a1e63828bd34a6e640b2149f04679c14d91083
mmanikandap0512 <manikandaprabhu0512@gmail.com>
2026-03-26 12:26:15 +0530

    refactor: define PlaygroundItem type and remove implicit any in map

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default async function DashboardLayout({
     ANGULAR: "Terminal",
   };
 
-  const formattedPlaygroundData = playgroundData?.map((item) => ({
+  const formattedPlaygroundData = playgroundData?.map((item: any) => ({
     id: item.id,
     name: item.title,
     starred: item.Starmark?.[0]?.isMarked || false,
